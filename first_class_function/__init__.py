887329017af6d1c88290865f3dd726f49f9261f4
adair_chan <adairchan.dream@gmail.com>
2019-09-19 17:43:34 +0800

    fix(Update __init__.py):

--- first_class_function/__init__.py
+++ first_class_function/__init__.py
@@ -4,3 +4,24 @@
 # @Email   : adairchan.dream@gmail.com
 # @Date    : 2019/8/8 上午9:51
 # @IDE     : PyCharm
+import random
+
+
+class BingoCage:
+    def __init__(self, items):
+        self._items = list(items)
+        random.shuffle(self._items)
+
+    def pick(self):
+        try:
+            return self._items.pop()
+        except IndexError:
+            raise LookupError('pick from empty BingoCage')
+
+    def __call__(self):
+        return self.pick()
+
+
+if __name__ == '__main__':
+    bingo = BingoCage(range(3))
+    bingo.pick()
