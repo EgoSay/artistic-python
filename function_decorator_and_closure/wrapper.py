596c2c41dd42a85d67d9b8d11064b3519824e6ac
adair_chan <adairchan.dream@gmail.com>
2019-09-24 10:58:33 +0800

    fix(函数装饰器与闭包): 装饰器
    添加装饰器：输出程序运行信息

--- function_decorator_and_closure/wrapper.py
+++ function_decorator_and_closure/wrapper.py
@@ -5,6 +5,8 @@
 # @Date    : 2019/9/19 下午4:40
 # @IDE     : PyCharm
 """装饰器模块"""
+import time
+import functools
 registry = list()
 
 
@@ -23,6 +25,26 @@ def test_order(func):
         func()
         return registry
     return wrapper
+
+
+def clock(func):
+    """在每次调用被装饰的函数时计时，然后把经过的时间、传入的参数和调用的结果打印出来"""
+    @functools.wraps(func)
+    def clocked(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        elapsed = time.time() - t0
+        name = func.__name__
+        arg_lst = []
+        if args:
+            arg_lst.append(', '.join(repr(arg) for arg in args))
+        if kwargs:
+            pairs = ['%s=%r' % (k, w) for k, w in sorted(kwargs.items())]
+            arg_lst.append(', '.join(pairs))
+        arg_str = ', '.join(arg_lst)
+        print('[%0.8fs] %s(%s) -> %r ' % (elapsed, name, arg_str, result))
+        return result
+    return clocked
 
 
 
