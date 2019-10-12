499dd4bbe888ad0c9f1cf0419742bab8f9e2c09c
adair_chan <adair_chan@hotmail.com>
2019-10-12 18:31:54 +0800

    feat(迭代器与生成器): A Example to test iteration

---
+++ iterators_and_generators/iteration_v1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/10/12 12:09 下午
+# @IDE     : PyCharm
+import re
+import reprlib
+
+
+class Example1:
+    """定义一个示例类, 通过索引从文本中提取单词, 它是一个可迭代对象"""
+
+    def __init__(self, text):
+        self.text = text
+        # 匹配提取所有数字字母字符,  re 模块用法见: https://github.com/python/cpython/blob/3.7/Lib/re.py
+        self.words = re.compile('\w+').findall(text)
+
+    def __getitem__(self, index):
+        return self.words[index]
+
+    def __len__(self):
+        return len(self.words)
+
+    def __repr__(self):
+        """reprlib.repr 这个实用函数用于生成大型数据结构的简略字符串表示形式"""
+        return '(%s)' % reprlib.repr(self.text)
+
+
+if __name__ == '__main__':
+    e1 = Example1("hello world, i use 'python'、'Java', it's beautiful ")
+    print(e1)
+    """测试这个类示例可以迭代"""
+    for word in e1:
+        print(word)
