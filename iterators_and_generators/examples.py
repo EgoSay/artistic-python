1a85ee1959f7f77886697c8d0d243690a975eea9
adair_chan <adair_chan@hotmail.com>
2019-10-12 10:40:21 +0800

    feat(迭代器与生成器): 生成器只能遍历一次

--- iterators_and_generators/examples.py
+++ iterators_and_generators/examples.py
@@ -4,6 +4,7 @@
 # @Email   : adairchan.dream@gmail.com
 # @Date    : 2019/10/11 6:31 下午
 # @IDE     : PyCharm
+import numpy as np
 
 
 def fib():
@@ -14,7 +15,26 @@ def fib():
         curr, prev = prev + curr, curr
 
 
-f = fib()
-for i in range(10):
-    print(next(f))
+def cal(array):
+    """验证生成器只能遍历一次"""
+    for item in array:
+        yield item
 
+
+if __name__ == '__main__':
+
+    print("""========== example1, 斐波那契数列==========""")
+    f = fib()
+    for i in range(10):
+        print(next(f))
+
+    print("""======== example2, 验证生成器只能遍历一次==========""")
+    arr = np.random.randint(100, size=8)
+    print("生成随机数组为:{0}".format(arr))
+    result = cal(arr)
+    print("下面是 sum 求和会遍历一次生成器👇")
+    all_sum = sum(result)
+    print(all_sum)
+    print("第二次遍历生成器， 没有任何输出打印")
+    for r in result:
+        print(r / all_sum)
